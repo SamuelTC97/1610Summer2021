1f06207f7005b5e13c5a6ab606fa359af6b4e757
SamuelTC97 <samueltrevor.carter@gmail.com>
2021-06-04 19:38:00 -0600

    MouseClick

---
+++ DGM 1610 Summer 2021/Assets/MouseClick.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    void OnMouseDown ()
+    {
+        rigidbody.AddForce(-transform.forward * 500f);
+        rigidbody.useGravity = true;
+    }
+}
