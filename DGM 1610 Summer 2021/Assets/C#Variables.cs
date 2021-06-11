b683214ecf6928975e0f00d841eaf4e9f9cc8877
SamuelTC97 <samueltrevor.carter@gmail.com>
2021-06-11 12:16:22 -0600

    C#Variables

---
+++ DGM 1610 Summer 2021/Assets/C#Variables.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C#Variables : MonoBehaviour
+{
+	{
+		int myNum = 4;
+	}
+	
+
+	{
+		double myDoubleNum = 5.99D;
+	}
+
+
+	{
+		char myLetter = 'D';
+	}
+
+
+	{
+		bool myBool	= true;
+	}
+
+
+	{
+		string myText = "Happy";
+	}
+}
