57caac97d354660d95e3528d65a3462e84d09d8a
SamuelTC97 <samueltrevor.carter@gmail.com>
2021-06-19 17:18:05 -0600

    IfElse

---
+++ DGM 1610 Summer 2021/Assets/IfElse.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfElse : MonoBehaviour
+{
+	int x = 50;
+	int y = 10;
+	if (x > y);
+	{
+		Console.WriteLine("Hello World");
+	}
+}
+{
+	int x = 50;
+	int y = 50;
+	if (x == y);
+	{
+		Console.WriteLine("Hello World");
+	}
+}
+{
+	int x = 50;
+	int y = 50;
+	if (x == y);
+	{
+		Console.WriteLine("Yes");
+	}
+	else
+	{
+		Console.WriteLine("No");
+	}
+}
+{
+	int x = 50;
+	int y = 50;
+	if (x == y);
+	{
+		Console.WriteLine("1");
+	}
+	else if (x > y);
+	{
+		Console.WriteLine("2");
+	}
+	else
+	{
+		Console.WriteLine("3");
+	}
+}
+{
+	int time = 20;
+	string result = (time < 18) ? "Good Day." : " Good Evening.";
+	Console.WriteLine(result);
+}
